1c232f5252dc8de24945766e6f35331379a7ebd4
vamsee9 <4n0n5ur7@protonmail.com>
2021-02-15 19:57:48 +0530

    first commit

---
+++ HTTPServer.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+
+# Defining PORT number
+PORT = 8080
+
+# Creating handle
+Handler = http.server.SimpleHTTPRequestHandler
+
+# Creating TCPServer
+http = socketserver.TCPServer(("", PORT), Handler)
+
+# Getting logs
+print("serving at port", PORT)
+http.serve_forever()
